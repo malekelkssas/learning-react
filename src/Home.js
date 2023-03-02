7010208d2c333777a5cc56d02102e487ef5cf5a7
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-03-02 14:02:53 +0200

    Lesson 8 :using (useState) and make reactive values

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,5 @@
+import { useState } from "react"; // this library is to make a vriable reactive => change in browser whenever change in react
+
 const Home = () => {
 // function without arguments is easy to be use (the argument here is event object)
     const handleclick = (event) =>
@@ -10,6 +12,14 @@ const Home = () => {
     {
         console.log('hi there',name , event.target);
     }
+
+    const [name, setName] = useState('malek'); //this how we make a value reactive [value , set function]
+    const changeName = () =>{
+        let tmp = Math.random()*5;
+        let tmp2 = ['malek','mohamed','nour','eldean','elkassas']
+        setName(tmp2[parseInt(tmp)]);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
@@ -17,6 +27,8 @@ const Home = () => {
             <button onClick={(event) =>{
                 handleclickagain('malek',event);
             }}>click me</button>
+            <p> { name } </p>
+            <button onClick={changeName}>change the name</button>
         </div>
      );
 }
