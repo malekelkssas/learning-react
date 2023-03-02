44748b2c3c699e27bddbe05c0de58163210e6109
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-03-02 13:05:37 +0200

    lesson 6

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return ( 
+        <nav className="Navbar">
+            <h1>Welcome</h1>
+            <div>
+                <a href="/">Home</a>
+                <a href="/create">new page</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
+
+/*
+* to create function faster use (sfc)
+ */
