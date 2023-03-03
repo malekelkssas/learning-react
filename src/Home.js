67cc4fc7ef355918c8688aca6e70276e9952a1d4
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-03-03 15:58:17 +0200

    lesson 14&15 : useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,12 @@
-import { useState } from "react"; // this library is to make a vriable reactive => change in browser whenever change in react
+import { useState ,useEffect } from "react"; // now we will use effect
 import BlogList from "./BlogList";
 
 /**
- * here i will reuse the compnents with filters
+ * usestate is something called hook
+ * there is another hooks
+ * now we will use useeffect => it run function everytime we render the element
+ *                              and this happen every time you refresh or first load
+ *                              and when state (useState) changes
  */
 
 const Home = () => {
@@ -11,6 +15,7 @@ const Home = () => {
         let tmp = blogs.filter(element => element.id !== id);
         setBlogs(tmp);
     };
+
     const [blogs , setBlogs] = useState(
         [
             { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
@@ -18,10 +23,24 @@ const Home = () => {
             { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
         ]
     );
+        const [name,setNAme] = useState("malek");
+        useEffect(()=>{
+            // take care of changing state inside this
+            console.log("hi malek");
+            console.log(name);
+            // as the page can be re-render many times
+            // and i may don't need this function to run
+            // every tim i will somthing called dependecy array
+            // we pass it as arguments => sothat it runs just after 
+            // the first render (if it is empty)
+            // or i can put a dependency 
+        },[name]);
 
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title = "all blogs" deleteBlog = {deleteBlog} />
+            <button onClick={()=>setNAme("mo")}>change</button>
+            <p>{name}</p>
         </div>
      );
 }
