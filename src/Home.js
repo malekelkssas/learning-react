ac954d1dd255d3cdae7143eeb0a0e640b21c514c
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-03-04 23:23:48 +0200

    Lesson 19: Handling Fetch Errors

--- src/Home.js
+++ src/Home.js
@@ -2,9 +2,7 @@ import { useState ,useEffect } from "react";
 import BlogList from "./BlogList";
 
 /**
- * as featching data from server may take sometime (not like json file here)
- * we will use conditions again to make
- * conditional messege
+ * handling featching errors
  */
 
 const Home = () => {
@@ -13,25 +11,38 @@ const Home = () => {
         let tmp = blogs.filter(element => element.id !== id);
         setBlogs(tmp);
     };
-
+    
     const [blogs , setBlogs] = useState(null);
+    const [isPending,setIsPending] = useState(true);
+    const [error,setError] = useState(null);
+
         useEffect(()=>{
             // i will use settimeout to act like a server
             setTimeout(() => {
-               fetch('http://localhost:8000/blogs').then(res => {
+            fetch('http://localhost:8000/blogs').then(res => {
+                console.log(res);
+                if(!res.ok)
+                {
+                    throw Error('coudn not fetch the data from the server :(')
+                }
                 return res.json();
             }).then( data =>{
                 setBlogs(data);
                 setIsPending(false);
+                setError(null);
+            }).catch((err)=>{
+                //console.log(err.message); // this can fetch any error generally 
+                setError(err.message);
+                setIsPending(false);
             }); 
-            }, 1000);
+            }, 1000)
             
         },[]);
 
-    const [isPending,setIsPending] = useState(true);
 
     return ( 
         <div className="home">
+            {error && <div>{ error }</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title = "all blogs" deleteBlog = {deleteBlog} />} {/**this is to check if blogs is null or not */}
         </div>
