3b1077baeab80d680d92d87f92ad5cf6144212b7
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-03-10 01:17:03 +0200

    lesson 23 : using link instead of <a href>

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import Navbar from './Navbar';
 import Home from './Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Create from './Create';
-// I will learn routs using react
-/**
- * 1-first we suround our entiry application with Router
- * 2- make Switch to each page (where is the new content will be
- *  put ) => in this example it will be put after the div of className = content
- *  it also ensure that only one route show in one time
- * 3-set our routes for each page
- * *notice that the Navbar always in show because it is not in the switch
- */
+import Create from './create';
+
+
 
 function App() {
   return (
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,18 @@
+import { Link } from "react-router-dom";
 
+// making react handling the get request instead of getting it from the server
+// just instead of a => use Link
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Welcome</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color:'beige',
                     backgroundColor:"violet",
                     borderRadius:'8px'
-                }}>new page</a>
+                }}>new page</Link>
             </div>
         </nav>
      );
