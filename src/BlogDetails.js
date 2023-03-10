4d14be802cc417dd98552da42556eadd8a445cf9
Malek Mohamed Noureldean Elkssas <87043730+malekelkssas@users.noreply.github.com>
2023-03-10 15:09:12 +0200

    Lesson 31 : remove from database (delete request)

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,12 +1,21 @@
 import { useParams } from "react-router-dom"; 
 import useFetch from "./useFetch";
+import { useHistory }  from "react-router-dom" ;
 
-//now i will use useFetch again
 
 const BlogDetails = () => {
     const {id} = useParams();
     const {data,isPending,error} = useFetch('http://localhost:8000/blogs/' + id);
-    //important => this id in the previous line which make the data fetched correctly
+
+    const history = useHistory();
+
+    const handleClick = () => {
+         fetch('http://localhost:8000/blogs/'+id,
+         {
+            method:'DELETE',
+         }
+         ).then(()=> history.push('/'));
+    }
 
     return ( 
 <div className="blog-details">
@@ -17,7 +26,7 @@ const BlogDetails = () => {
             <h2>{data.title}</h2>
             <p>Written by {data.author}</p>
             <div>{data.body}</div>
-            
+            <button onClick={handleClick}>Delete</button>
         </article>
     )}
 </div>
